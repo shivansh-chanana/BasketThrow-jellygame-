e51fd415ccdf03f37a2f97a61cef10721479b61d
shivansh-chanana <shivansh.chanana@gmail.com>
2018-04-24 21:40:56 +0530

    BasketThrow v1.1
    *IP*
    Made 3 levels

---
+++ Basket throw game/Assets/Scripts/Sc_PlayerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sc_PlayerScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		//if it collides with portal
+		if (col.gameObject.tag == "Portal")
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
---
+++ Basket throw game/Assets/Scripts/Sc_Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_Portal : MonoBehaviour {
+
+	public Rigidbody2D rb;
+
+	//Rotation Speed
+	public float rotSpeed = 1f; 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.transform.Rotate (0,0,rotSpeed);
+	}
+}
